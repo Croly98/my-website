5f8c39e90602369e2358d7b3defafc170da64ef7
Josh Croly <jcroly1998@gmail.com>
2025-07-23 21:42:12 +0100

    fixed card issue, card looks good just need to add blue border + fix text

--- src/Card.jsx
+++ src/Card.jsx
@@ -6,11 +6,17 @@ import './card.css'
 function Card(){
     return (
     <div className="Card">
-        <img src={profilePic} alt="fox profile picture"></img>
+        <img src={profilePic} alt="fox profile picture"></img> 
+    
+    <div className="card-name">
         <h2>Josh</h2>
+    </div>   
         
     </div>
     );
 }
+
+//i could have it instead of entering a password you put in your name!
+// which is used to thank them for donwloading cv, loading webpage etc
 
 export default Card
